14daa57508c98c8bb565ae260c89bcaeaee0683b
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-08-06 18:44:55 -0300

    add atividade 01 03

--- IMD0509/Aula03Atividade01/Aula03Atividade03/app/src/main/java/com/felipetiberio/aula03atividade03/Main2Activity.kt
+++ IMD0509/Aula03Atividade01/Aula03Atividade03/app/src/main/java/com/felipetiberio/aula03atividade03/Main2Activity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //var button = findViewById<Button>(button1)
+        //var button = findViewById<Button>(button2)
+
     }
 
 
--- IMD0509/Aula03Atividade01/Aula03Atividade03/app/src/main/java/com/felipetiberio/aula03atividade03/MainActivity.kt
+++ IMD0509/Aula03Atividade01/Aula03Atividade03/app/src/main/java/com/felipetiberio/aula03atividade03/MainActivity.kt
@@ -5,21 +5,31 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
+import android.widget.Button
+import com.felipetiberio.aula03atividade03.R.id.button1 as button1
 
 class MainActivity : AppCompatActivity() {
 
-   
+    var i = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var clicou = false
+        var button = findViewById<Button>(button1)
+
+
+        button.text = i.toString()
     }
 
 
     fun next (view: View){
-        val intent = Intent(this, Main2Activity::class.java)
+       val intent = Intent(this, Main2Activity::class.java)
 
+        i++
         startActivityForResult(intent, 1)
+
+
     }
 
 
